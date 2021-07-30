d655221e9f94d7fd27e073ecaea4b35f75607d23
gertvandormael <vandormael.gert@gmail.com>
2021-07-30 21:30:25 +0200

    Add api call to headless cms for input box

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,60 @@
 <template>
   <section class="section">
     <div class="content">
-      <h1 class="title is-1">Dynamic forms</h1>
-      <NuxtLink to="/overview" exact-active-class="is-active">
-        Home page is still in progress visit elements overview
-      </NuxtLink>
+      <h1 class="title is-1">{{ data._embedded.content[0].name }}</h1>
+      <InputBox
+        :input-label="data._embedded.content[0].inputLabel"
+        :input-type="data._embedded.content[0].inputType"
+        :input-status="inputStatus"
+        :input-icon="
+          data._embedded.content[0].hasInputIcon
+            ? data._embedded.content[0]
+            : ''
+        "
+      />
     </div>
   </section>
 </template>
 
 <script>
+import InputBox from '~/components/InputBox'
+
 export default {
-  name: 'HomePage'
+  name: 'HomePage',
+  components: {
+    InputBox,
+  },
+  async asyncData({ $axios }) {
+    let config = {
+      headers: {
+        'Accept-Language': 'en-US',
+        'umb-project-alias': 'gerts-versatile-gorilla',
+      },
+    }
+
+    const data = await $axios.$get(
+      `${$axios.defaults.baseURL}/content`,
+      config
+    )
+    return { data }
+  },
+
+  computed: {
+    inputStatus() {
+      let status = ''
+      switch (this.data._embedded.content[0].inputStatus) {
+        case 'success':
+          status = 'is-success'
+          break
+        case 'fail':
+          status = 'is-danger'
+          break
+        default:
+          status = ''
+      };
+
+      return status;
+    },
+  },
 }
 </script>
